cf89430d843a897b86553a7f8b682420e6c72037
alex <devkral@web.de>
2016-12-04 02:46:41 +0100

    qt5gui progress

--- chatscn/chatqt5/__init__.py
+++ chatscn/chatqt5/__init__.py
@@ -4,8 +4,10 @@ import os
 import sys
 import json
 import logging
+import time
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtQml import QQmlComponent, QQmlEngine
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
+#from PyQt5.QtQml import QQmlComponent, QQmlEngine
 from PyQt5.QtCore import QUrl, QStandardPaths
 
 from simplescn.scnrequest import Requester
@@ -26,27 +28,42 @@ def genHandler(rootwidget, chatdirectory):
             return self.root.senslevel == 2
     return QT5Handler
 
-class qt5chat(object):
+
+class ChatMainWin(QWidget):
     app = None
     engine = None
     requester = None
     basedir = None
-    def __init__(self, requester):
-        self.requester = requester
-        self.app = QApplication(sys.argv)
-        self.basedir = QStandardPaths.writableLocation(QStandardPaths.DataLocation)
-        # Create a QML engine.
-        self.engine = QQmlEngine()
+    def __init__(self, requester, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.requester = chatscn.SCNSender(requester, self.basedir)
+        self.initUI()
 
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+        self.basedir = os.path.join(QStandardPaths.writableLocation(QStandardPaths.ConfigLocation), "chatscn")
+        print(self.basedir)
+        title = QLabel('Title')
+        grid.addWidget(title, 0, 0)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')
+        self.activateWindow()
+        self.show()
+
+        # Create a QML engine.
+        #self.engine = QQmlEngine()
         # Create a component factory and load the QML script.
-        component = QQmlComponent(self.engine)
-        component.loadUrl(QUrl('example.qml'))
-        self.hserver = chatscn.init(requester, genHandler(self.app, self.basedir))
+        #component = QQmlComponent(self.engine)
+        #component.loadUrl(QUrl('example.qml'))
+
+        self.hserver = chatscn.init(self.requester, genHandler(self, self.basedir))
         if not self.hserver:
             raise
 
-    def run(self):
-        return self.app.exec_()
 def openchat(address, use_unix):
+    app = QApplication([])
     requester = Requester(addrcon=address, use_unix=use_unix)
-
+    ChatMainWin(requester)
+    app.exec_()
